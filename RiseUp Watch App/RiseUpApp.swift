bc0820ed275af21555b25285a2dec80b5406905a
littlered964 <bkrutz96@gmail.com>
2025-11-17 22:51:18 -0500

    Add initial watchOS RiseUp project

---
+++ RiseUp Watch App/RiseUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiseUpApp.swift
+//  RiseUp Watch App
+//
+//  Created by Bradley Krutz on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RiseUp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RiseUp Watch AppTests/RiseUp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RiseUp_Watch_AppTests.swift
+//  RiseUp Watch AppTests
+//
+//  Created by Bradley Krutz on 11/17/25.
+//
+
+import Testing
+@testable import RiseUp_Watch_App
+
+struct RiseUp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
